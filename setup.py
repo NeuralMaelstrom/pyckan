56a197bf07bae8c25e651bcc8f1670229b23930f
NeuralMaelstrom <159735933+NeuralMaelstrom@users.noreply.github.com>
2024-05-20 18:08:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,19 +5,28 @@ with open("README.md", "r", encoding="utf8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pykan",
+    name="pyckan",  # Updated package name
     version="0.0.5",
-    author="Ziming Liu",
-    author_email="zmliu@mit.edu",
+    author="NeuralMaelstrom",  # Update author name if necessary
+    author_email="your_email@example.com",  # Update author email if necessary
     description="Kolmogorov Arnold Networks",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    # url="https://github.com/kindxiaoming/",
+    url="https://github.com/NeuralMaelstrom/pyckan",  # Update repository URL
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'torch>=2.2.2',
+        'numpy>=1.24.4',
+        'scikit_learn>=1.1.3',
+        'setuptools>=65.5.0',
+        'sympy>=1.11.1',
+        'matplotlib>=3.6.2',
+        'tqdm>=4.66.2'
     ],
     python_requires='>=3.6',
 )
